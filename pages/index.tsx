9ce91a1e8293c40ce699fce4643b3158f48774d1
Leo Li <leo4life@163.com>
2023-10-27 17:22:51 -0400

    cool duck

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,84 @@
-import {usePrivy} from '@privy-io/react-auth';
-import Head from 'next/head';
-import Chirp from '../components/graphics/chirp';
+import { usePrivy } from "@privy-io/react-auth";
+import Head from "next/head";
+import Chirp from "../components/graphics/chirp";
+import { useEffect, useRef } from "react";
 
 export default function LoginPage() {
-  const {login} = usePrivy();
+  const { login } = usePrivy();
+  const chirpRef = useRef<HTMLDivElement>(null);
+
+  const handleLogin = () => {
+    if (chirpRef.current) {
+      chirpRef.current.style.animation =
+        "squishAndRotate 1s ease-in-out forwards";
+    }
+    setTimeout(login, 1000); // Wait for animation to complete
+  };
+
+  useEffect(() => {
+    if (chirpRef.current) {
+      chirpRef.current.addEventListener("animationend", () => {
+        if (chirpRef.current) chirpRef.current.style.animation = "";
+      });
+    }
+  }, []);
 
   return (
     <>
       <Head>
-        <title>Login · Privy</title>
+        <title>Login · Chirp</title>
       </Head>
+
+      <style jsx>{`
+    @keyframes pulse {
+      0%, 100% {
+        transform: scale(1);
+      }
+      20% {
+        transform: scale(0.95);
+      }
+      50% {
+        transform: scale(1.04);
+      }
+    }
+    
+
+    @keyframes squishAndRotate {
+      0% {
+        transform: scale(1);
+      }
+      10% {
+        transform: scale(0.9);
+      }
+      50% {
+        transform: scale(1.2);
+      }
+      100% {
+        transform: scale(1) rotate(720deg);
+      }
+    }
+
+    .chirp-login {
+      cursor: pointer;
+      transition: transform 0.3s ease-in-out;
+      animation: pulse 2s infinite;
+    }
+
+    .chirp-login:hover {
+      transform: scale(1.1);
+    }
+
+    .chirp-login:active {
+      animation: squishAndRotate 1s ease-in-out forwards;
+    }
+`}</style>
+
 
       <main className="flex min-h-screen min-w-full">
         <div className="flex bg-duck-light-yellow flex-1 p-6 justify-center items-center">
           <div>
-            <div>
+            <div className="chirp-login" onClick={handleLogin} ref={chirpRef}>
               <Chirp />
-            </div>
-            <div className="mt-6 flex justify-center text-center">
-              <button
-                className="bg-duck-beak hover:bg-duck-eye py-3 px-6 text-white rounded-lg"
-                onClick={login}
-              >
-                Log in
-              </button>
             </div>
           </div>
         </div>
